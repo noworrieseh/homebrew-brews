e3657a6cf1940a1ce10855e2d0ed20e7e7220758
Andrew Selkirk <alselkirk@gmail.com>
2026-01-01 21:41:45 -0500

    Add macdisp tool

---
+++ Formula/macdisp.rb
@@ -0,0 +1,12 @@
+class MacDisp < Formula
+  desc "Display configuration tool for macOS"
+  homepage "https://github.com/noworrieseh/macdisp"
+  url "https://github.com/noworrieseh/macdisp/releases/download/v0.2.1/macdisp-universal-v0.2.1.tar.gz"
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/macdisp"
+  end
+end
+
